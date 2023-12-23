0e627c371baf12a7965f8724aff8cb3678228cd9
James Boyer <jboyer87@users.noreply.github.com>
2023-12-23 05:29:29 -0800

    Clarify comment.

--- js/substack.js
+++ js/substack.js
@@ -57,7 +57,7 @@ async function displaySinglePostFromRssFeedByGuid(postGuid) {
 				<p class="substack-post-content">${content}</p>
 			`;
 
-        // Set the title of the page dynamically based on the post title.
+        // Set the title of the post page dynamically based on the post title.
         document.title = document.title.replace("{Post}", title);
 
         // Remove the last empty paragraph. Not sure why Substack sends this.
